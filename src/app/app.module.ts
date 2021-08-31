18113d116c6a47f5420914b0bf13d6654cfcd6b4
neiser.montano <neisermontano1991@gmail.com>
2021-08-30 23:52:03 -0500

    routing y rxjs susbcribe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module'; // CLI imports 
 import { AppComponent } from './app.component';
 import { EjerciciosComponent } from './ejercicios/ejercicios.component';
 import { EjercicioDetalleComponent } from './ejercicio-detalle/ejercicio-detalle.component';
@@ -13,7 +14,8 @@ import { EjercicioDetalleComponent } from './ejercicio-detalle/ejercicio-detalle
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
