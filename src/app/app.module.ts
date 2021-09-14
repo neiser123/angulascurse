388801e9550fc0534f0972c3f10411377a2e3132
neiser.montano <neisermontano1991@gmail.com>
2021-09-13 23:48:07 -0500

    routing y oulet router y redirreciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module'; // CLI imports
 import { AppComponent } from './app.component';
 import { EjerciciosComponent } from './ejercicios/ejercicios.component';
 import { EjercicioDetalleComponent } from './ejercicio-detalle/ejercicio-detalle.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EjerciciosComponent,
-    EjercicioDetalleComponent
+    EjercicioDetalleComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
